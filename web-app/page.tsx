a570eaefec418790fe42cce1c590a5f6cd895f1c
boardwalkclay1 <boardwalkclay1@gmail.com>
2025-12-24 00:35:26 -0500

    Create page.tsx

---
+++ web-app/page.tsx
@@ -0,0 +1,10 @@
+"use client";
+
+export default function Page() {
+  return (
+    <main className="min-h-screen p-6">
+      <h1>Page Title</h1>
+      <p>This page is working.</p>
+    </main>
+  );
+}
